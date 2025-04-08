d22b610a8db0ed13dae8d8a5f6f2e0fd42b6ac02
서현우 <tone8943>
2025-04-08 12:30:04 +0900

    Initial commit

---
+++ src/main/java/com/hwoolog/HwoologApplication.java
@@ -0,0 +1,13 @@
+package com.hwoolog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwoologApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwoologApplication.class, args);
+    }
+
+}
