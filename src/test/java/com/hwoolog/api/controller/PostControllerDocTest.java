5523395a82e65719078699b2603bc7dc3697496d
hwoo <tone8943@gmail.com>
2025-04-23 19:02:30 +0900

    Spring REST docs1 - 기본설정

--- src/test/java/com/hwoolog/api/controller/PostControllerDocTest.java
+++ src/test/java/com/hwoolog/api/controller/PostControllerDocTest.java
@@ -1,28 +1,37 @@
 package com.hwoolog.api.controller;
 
+import com.hwoolog.api.domain.Post;
+import com.hwoolog.api.repository.PostRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.http.MediaType.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 @ExtendWith(RestDocumentationExtension.class)
 public class PostControllerDocTest {
 
+    @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private PostRepository postRepository;
 
     @BeforeEach
     void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
@@ -32,12 +41,20 @@ public class PostControllerDocTest {
     }
 
     @Test
-    @DisplayName("test")
+    @DisplayName("글 단건 조회 테스트")
     void test1() throws Exception {
+        // given
+        Post post = Post.builder()
+                .title("foo")
+                .content("boo")
+                .build();
+        postRepository.save(post);
+
         // expected
-        this.mockMvc.perform(get("/")
+        this.mockMvc.perform(get("/posts/{postId}", post.getId())
                         .accept(APPLICATION_JSON))
                 .andExpect(status().isOk())
+                .andDo(print())
                 .andDo(document("index"));
     }
 }
