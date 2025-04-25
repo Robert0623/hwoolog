a3c7e2c26ecd8bababe8044416a603bab3323d67
hwoo <tone8943@gmail.com>
2025-04-25 21:16:36 +0900

    vue init

---
+++ front/src/views/WriteView.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+import {ref} from "vue";
+
+import axios from 'axios';
+
+const title = ref("")
+const content = ref("")
+
+const write = function () {
+  axios.post('http://localhost:8080/posts', {
+    title: title.value,
+    content: content.value
+  });
+};
+</script>
+
+<template>
+  <div>
+    <el-input v-model="title" type="text" placeholder="제목을 입력해주세요"/>
+  </div>
+
+  <div>
+    <el-input v-model="content" type="textarea" name="" id="" cols="30" rows="15"/>
+  </div>
+
+  <div class="mt-2">
+    <el-button type="primary" @click="write()">글 작성완료</el-button>
+  </div>
+</template>
+
+<style>
+
+</style>
