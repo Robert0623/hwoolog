7ef6449ad55424d98dd5aad024751fdda6091b91
hwoo <tone8943@gmail.com>
2025-04-25 23:36:17 +0900

    [FRONT] 글 내용 화면 만들기

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import axios from "axios";
 import {ref} from "vue";
+import {useRouter} from "vue-router";
+
+const router = useRouter();
 
 const posts = ref([]);
 
@@ -10,6 +13,7 @@ axios.get('/api/posts?page=1&size=5')
     posts.value.push(r);
   });
 })
+
 </script>
 
 <template>
@@ -17,7 +21,7 @@ axios.get('/api/posts?page=1&size=5')
   <ul>
     <li v-for="post in posts" :key="post.id">
       <div>
-        {{ post.title }}
+        <router-link :to="{name: 'read', params: {postId: post.id}}">{{ post.title }}</router-link>
       </div>
 
       <div>
