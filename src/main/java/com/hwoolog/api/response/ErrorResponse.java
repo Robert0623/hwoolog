45c596d19bf0784163986916b36c908fd3020b0e
hwoo <tone8943@gmail.com>
2025-05-11 22:26:28 +0900

    [API] error dto 생성자 수정

--- src/main/java/com/hwoolog/api/response/ErrorResponse.java
+++ src/main/java/com/hwoolog/api/response/ErrorResponse.java
@@ -3,6 +3,7 @@ package com.hwoolog.api.response;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -27,7 +28,7 @@ public class ErrorResponse {
     public ErrorResponse(String code, String message, Map<String, String> validation) {
         this.code = code;
         this.message = message;
-        this.validation = validation;
+        this.validation = validation != null ? validation : new HashMap<>();
     }
 
     public void addValidation(String fieldName, String errorMessage) {
