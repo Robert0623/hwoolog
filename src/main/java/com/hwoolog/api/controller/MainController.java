92d4501a233bd61de2be4f96e7c50b22b8653b48
hwoo <tone8943@gmail.com>
2025-05-11 22:33:39 +0900

    [AUTH] 시큐리티 로그인 사용자 정보 가져오기

--- src/main/java/com/hwoolog/api/controller/MainController.java
+++ src/main/java/com/hwoolog/api/controller/MainController.java
@@ -1,5 +1,7 @@
 package com.hwoolog.api.controller;
 
+import com.hwoolog.api.config.UserPrincipal;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +14,7 @@ public class MainController {
     }
 
     @GetMapping("/user")
-    public String user() {
+    public String user(@AuthenticationPrincipal UserPrincipal userPrincipal) {
         return "사용자 페이지입니다. 😊";
     }
 
